f519da62c06b959ba3ccaf1bece5898d544f09c7
JSigne <signe.josie@gmail.com>
2023-01-08 22:51:25 +0100

    API Solution initiale

---
+++ api/src/test/java/com/astella/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astella.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
